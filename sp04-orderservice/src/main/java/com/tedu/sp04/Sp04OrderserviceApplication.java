be44d1bd0c9cceb1dc96f1fd3ab49fba689cb2eb
yablei <53455915+yablei@users.noreply.github.com>
2019-07-30 19:07:08 +0800

    Add files via upload

---
+++ sp04-orderservice/src/main/java/com/tedu/sp04/Sp04OrderserviceApplication.java
@@ -0,0 +1,14 @@
+package com.tedu.sp04;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+@EnableDiscoveryClient
+@SpringBootApplication
+public class Sp04OrderserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sp04OrderserviceApplication.class, args);
+	}
+
+}
