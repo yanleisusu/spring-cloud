a8c58db31e24fb4a94ccaaee4b8580fc84268d34
yablei <53455915+yablei@users.noreply.github.com>
2019-07-31 09:40:46 +0800

    Add files via upload

---
+++ sp06-ribbon/src/main/java/com/tedu/sp06/Sp06RibbonApplication.java
@@ -0,0 +1,22 @@
+package com.tedu.sp06;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+@EnableDiscoveryClient
+@SpringBootApplication
+public class Sp06RibbonApplication {
+	//创建 RestTemplate 实例，并存入 spring 容器
+	   @LoadBalanced //负载均衡注解
+		@Bean
+		public RestTemplate getRestTemplate() {
+			return new RestTemplate();
+		}
+	public static void main(String[] args) {
+		SpringApplication.run(Sp06RibbonApplication.class, args);
+	}
+
+}
